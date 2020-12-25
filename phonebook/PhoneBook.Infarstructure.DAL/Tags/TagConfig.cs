6c3dab173614cd4a6ed9adff99a25627a98b50a5
benito lsi <maryam_l89@yahoo.com>
2020-12-25 20:09:57 +0330

    Add Infrastructures.DAL

---
+++ phonebook/PhoneBook.Infarstructure.DAL/Tags/TagConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PhoneBook.Domain.Core.Tags;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBook.Infarstructure.DAL.Tags
+{
+    internal class TagConfig : IEntityTypeConfiguration<Tag>
+    {
+        public void Configure(EntityTypeBuilder<Tag> builder)
+        {
+            builder.Property(c => c.Title).HasMaxLength(20);
+        }
+    }
+}
---
+++ phonebook/PhoneBook.Infarstructure.DAL/phones/Persons/PersonConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using PhoneBook.Domain.Core.Peoples;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PhoneBook.Infarstructure.DAL.Persons
+{
+    internal class PersonConfig : IEntityTypeConfiguration<Person>
+    {
+        public void Configure(EntityTypeBuilder<Person> builder)
+        {
+            builder.Property(c => c.FristName).HasMaxLength(50).IsRequired(true);
+            builder.Property(c => c.LastName).HasMaxLength(50).IsRequired(true);
+            builder.Property(c => c.Address).HasMaxLength(550).IsRequired(true);
+            builder.Property(c => c.Email).HasMaxLength(250).IsRequired(true);
+            builder.Property(c => c.Image).IsUnicode(false);
+        }
+    }
+}
